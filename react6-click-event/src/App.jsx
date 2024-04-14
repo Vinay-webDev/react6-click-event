57478835548d6b26e5dca61fee376798ee080c1b
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-04-14 23:53:14 +0530

    click-event ==>> onClick event handler

---
+++ react6-click-event/src/App.jsx
@@ -0,0 +1,17 @@
+/* click-event = An interaction when a user
+                 clicks on a specific element.
+                 we can respond to clicks by 
+                 a callback to the onClick 
+                 event handler. */
+import Button from './Button.jsx'
+function App() {
+  
+
+  return (
+    <>
+      <Button/>
+    </>
+  )
+}
+
+export default App
---
+++ react6-click-event/src/Button.jsx
@@ -0,0 +1,18 @@
+ 
+function Button() {
+
+    const handleClick = console.log("ouch!");
+    return(
+        <button onClick={handleClick} > click me😃 </button>
+    );
+}
+
+export default Button
+
+
+
+
+
+
+
+
