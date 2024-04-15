4ee13ced7e76568d2f2d8c71c5f2eeefac7afebc
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-04-15 23:51:40 +0530

    new example using conditionals
    click event using conditionals

--- react6-click-event/src/Button.jsx
+++ react6-click-event/src/Button.jsx
@@ -1,12 +1,21 @@
  
 function Button() {
 
-    const handleClick = console.log("ouch!");
-    const handleClick2 = (name) => console.log(`&{name} stop clicking me😤`)
+    let count = 0;
+    
+    const handleClick = (name) => {
+        if (count < 3) {
+            count++;
+            console.log(`${name} you clicked me ${count} times😃`);
+        } else {
+            console.log(`${name} stop clicking me😤`)
+        }
+    }
+
     return(
         <>
         {/*<button onClick={handleClick} > click me😃 </button>*/}
-        <button onClick={()=> handleClick2("dude")} > click me😃 </button>
+        <button onClick={() => handleClick("dude")} > click me😃 </button>
         </>
     );
 }
