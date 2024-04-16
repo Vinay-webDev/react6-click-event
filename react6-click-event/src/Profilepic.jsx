157c6ed6b5268d04540fe4712957d02bffdb712c
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-04-16 23:04:08 +0530

    using handleClick for image

--- react6-click-event/src/Profilepic.jsx
+++ react6-click-event/src/Profilepic.jsx
@@ -2,13 +2,17 @@
 function Profilepic() {
     
     const imageUrl = './src/assets/profilepic.jpg'
+    
+    const handleClick = () => console.log("ouch!")
+
     return(
-        <img src={imageUrl} ></img>
+        <img onClick={handleClick} src={imageUrl} ></img>
     );
 }
 
 export default Profilepic
 
 
-
+/* let's add event parameter to hide the profilepic 
+when we click */
 
